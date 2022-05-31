1578929ed93ec9451abce5b71c97a4130b77ecdb
yudiz_shakil <shakil.d@yudiz.in>
2022-05-31 18:17:03 +0530

    update reward percentage validation

--- globals/lib/helper.js
+++ globals/lib/helper.js
@@ -328,7 +328,7 @@ _.validateRewardPercentage = function(nReward) {
     if (nReward > 100 || nReward <= 0) return false;
     if (nReward >= 1 && 100 % nReward === 0) return true;
     if (nReward < 1 && nReward > 0) {
-        const _nReward = Number(nReward.toFixed(2));
+        const _nReward = Number(Number(nReward.toFixed(2)));
         if (100 % _nReward === 0 || Number((100 % _nReward).toFixed(2)) === _nReward) return true;
     }
     return false;
